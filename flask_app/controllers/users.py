5a6db89532a41c0356c49407289c090445c0c115
Brenda Huemer <brendahuemer@gmail.com>
2023-09-28 13:29:16 -0400

    act

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -1,10 +1,6 @@
 from flask_app import app
 from flask import render_template, request, redirect, flash, session
 from flask_app.models.user import User
-from flask_bcrypt import Bcrypt
-bcrypt = Bcrypt(app)
-import pymysql
-db = pymysql.connect(host = 'localhost', user = 'root', password = '', db = 'lab_bd')
 
 @app.route("/")
 def show_index():
